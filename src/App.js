d6640afbab2632ffdfeb5e8689e58680c9f3c6bc
RyanJRasmussen <ryan.james.rasmussen@gmail.com>
2022-07-27 19:48:35 -0500

    Added profile component to display some user info, started on bet-placing modal

--- src/App.js
+++ src/App.js
@@ -1,63 +1,63 @@
 //import logo from "./logo.svg";
 import "./App.css";
-import {createTheme, ThemeProvider} from "@mui/material/styles";
-import {CssBaseline} from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
 import Navbar from "./components/Navbar";
 import AuthenticationButton from "./components/AuthenticationButton";
-import { Auth0Provider } from "@auth0/auth0-react";
+import PlaceBetModal from "./components/PlaceBetModal";
 
 function App() {
-    //change up the MUI theme
-    const theme = createTheme({
-        palette: {
-            mode: "dark",
-            primary: {
-                main: "#274c77",
-            },
-            secondary: {
-                main: "#0dab44",
-            },
+  //change up the MUI theme
+  const theme = createTheme({
+    palette: {
+      mode: "dark",
+      primary: {
+        main: "#274c77",
+      },
+      secondary: {
+        main: "#0dab44",
+      },
+    },
+    // typography: {
+    //     fontSize: 14,
+    //     fontFamily: "nudista-web, sans-serif",
+    // },
+    shape: {
+      borderRadius: 10,
+    },
+    components: {
+      // Name of the component
+      MuiButtonBase: {
+        defaultProps: {
+          // The props to change the default for.
+          disableRipple: true, // No more ripple, on the whole application 💣!
+          style: {
+            textTransform: "none",
+            // color: "white",
+            // borderRadius: 20,
+          },
         },
-        // typography: {
-        //     fontSize: 14,
-        //     fontFamily: "nudista-web, sans-serif",
-        // },
-        shape: {
-            borderRadius: 10,
+      },
+      MuiTab: {
+        defaultProps: {
+          style: {
+            color: "white",
+            textTransform: "none",
+          },
         },
-        components: {
-            // Name of the component
-            MuiButtonBase: {
-                defaultProps: {
-                    // The props to change the default for.
-                    disableRipple: true, // No more ripple, on the whole application 💣!
-                    style: {
-                        textTransform: "none",
-                        // color: "white",
-                        // borderRadius: 20,
-                    },
-                },
-            },
-            MuiTab: {
-                defaultProps: {
-                    style: {
-                        color: "white",
-                        textTransform: "none",
-                    },
-                },
-            },
-        },
-    });
+      },
+    },
+  });
 
-    return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <Navbar />
-                  <AuthenticationButton />
-                hello world!
-        </ThemeProvider>
-    );
-
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Navbar />
+      <AuthenticationButton />
+      hello world!
+      <PlaceBetModal />
+    </ThemeProvider>
+  );
 }
 
 export default App;
