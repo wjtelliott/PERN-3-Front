f43e91e1dfd7f0ca974ecef8465908d9d5d3527a
wjtelliott <wjtelliott@gmail.com>
2022-07-30 11:34:20 -0500

    Changed redirect url to profile confirm page

--- src/index.js
+++ src/index.js
@@ -7,13 +7,14 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+const redirectUri = window.location.origin + '/profile/confirm';
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   
   <Auth0Provider
     domain={domain}
     clientId={clientId}
-    redirectUri={window.location.origin}
+    redirectUri={redirectUri}
   >
     <App />
   </Auth0Provider>
