79bf75762487a968b7653aec3a57b9658abe0d77
prashantkumar1113 <prashantkumar1113@gmail.com>
2022-07-31 15:39:04 -0500

    Added the Navbar to App.js to cleanup duplications

--- src/components/BetCardGrid.js
+++ src/components/BetCardGrid.js
@@ -1,35 +1,36 @@
 import React from "react";
 import GameCard from "./GameCard";
-import { Grid } from "@mui/material";
+import {Grid} from "@mui/material";
 import "../App.css";
-import { mlbData } from "./Data-Handling/TestData";
+import {mlbData} from "./Data-Handling/TestData";
 // import { mlbData } from "./Data-Handling/TestDataMLBOnly";
 export default function BetCardGrid() {
-  return (
-    <Grid
-      container
-      spacing={4}
-      justify="center"
-      justifyContent="space-around"
-      className="bettingGridItem"
-    >
-      {mlbData.slice(0, 8).map((game, i) => (
+    return (
         <Grid
-          item
-          xs={12}
-          sm={6}
-          md={4}
-          xl={3}
-          sx={{
-            justifyContent: "center",
-            display: "flex",
-            justifySelf: "center",
-          }}
-          style={{ textAlign: "center" }}
+            container
+            spacing={4}
+            justify="center"
+            justifyContent="space-around"
+            className="bettingGridItem"
+            // sx={{marginTop: "-150px"}}
         >
-          <GameCard gameData={game} />
+            {mlbData.slice(0, 8).map((game, i) => (
+                <Grid
+                    item
+                    xs={12}
+                    sm={6}
+                    md={4}
+                    xl={3}
+                    sx={{
+                        justifyContent: "center",
+                        display: "flex",
+                        justifySelf: "center",
+                    }}
+                    style={{textAlign: "center"}}
+                >
+                    <GameCard gameData={game} />
+                </Grid>
+            ))}
         </Grid>
-      ))}
-    </Grid>
-  );
+    );
 }
--- src/components/Home/Landing.jsx
+++ src/components/Home/Landing.jsx
@@ -1,97 +1,97 @@
-import React, { useEffect } from "react";
+import React, {useEffect} from "react";
 import Navbar from "../Shared/Navbar";
-import { Typography } from "@mui/material";
-import { Box } from "@mui/system";
-import { Link } from "react-router-dom";
+import {Typography} from "@mui/material";
+import {Box} from "@mui/system";
+import {Link} from "react-router-dom";
 import BetCardGrid from "../BetCardGrid";
 
 const LandingPage = () => {
+    // If we want to use AOS, we will have to have this line in all pages that use it
+    useEffect(() => {
+        eval(`AOS.init()`);
+    }, []);
 
-  // If we want to use AOS, we will have to have this line in all pages that use it
-  useEffect(()=>{eval(`AOS.init()`)},[]);
+    return (
+        <div>
+            <Box
+                sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                }}
+            >
+                <Typography
+                    variant="h2"
+                    align="center"
+                    sx={{
+                        margin: 3,
+                        display: {xs: "none", md: "flex"},
+                        fontWeight: 700,
+                        color: "inherit",
+                        textDecoration: "none",
+                    }}
+                >
+                    YouBetcha!
+                </Typography>
+            </Box>
 
-  return (
-    <div>
-      <Navbar />
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-        }}
-      >
-        <Typography
-          variant="h2"
-          align="center"
-          sx={{
-            margin: 3,
-            display: { xs: "none", md: "flex" },
-            fontWeight: 700,
-            color: "inherit",
-            textDecoration: "none",
-          }}
-        >
-          YouBetcha!
-        </Typography>
-      </Box>
-
-      <Box
-        sx={{
-          display: "flex",
-          flexWrap: "wrap",
-          justifyContent: "center"
-        }}
-      >
-        <Box
-          sx={{
-            display: "flex",
-            flexWrap: "wrap",
-            justifyContent: "center",
-            width: '90%'
-          }}
-          data-aos='fade-up'
-        >
-          Games to bet on
-          <BetCardGrid />
-        </Box>
-        <Box
-          sx={{
-            display: "flex",
-            flexWrap: "wrap",
-            justifyContent: "center",
-            width: '90%'
-          }}
-          data-aos='fade-up'
-        >
-          Games to lose on:
-          <BetCardGrid />
-        </Box>
-        <Box
-          sx={{
-            display: "flex",
-            flexWrap: "wrap",
-            justifyContent: "center",
-            width: '90%'
-          }}
-          data-aos='fade-up'
-        >
-          Games games games
-          <BetCardGrid />
-        </Box>
-        <Box
-          sx={{
-            display: "flex",
-            flexWrap: "wrap",
-            justifyContent: "center",
-            width: '90%'
-          }}
-          data-aos='fade-up'
-        >
-          Some more games
-          <BetCardGrid />
-        </Box>
-      </Box>
-    </div>
-  );
+            <Box
+                sx={{
+                    display: "flex",
+                    flexWrap: "wrap",
+                    justifyContent: "center",
+                }}
+            >
+                <Box
+                    sx={{
+                        display: "flex",
+                        flexWrap: "wrap",
+                        justifyContent: "center",
+                        width: "90%",
+                    }}
+                    data-aos="fade-up"
+                >
+                    Games to bet on
+                    <BetCardGrid />
+                </Box>
+                <Box
+                    sx={{
+                        display: "flex",
+                        flexWrap: "wrap",
+                        justifyContent: "center",
+                        width: "90%",
+                    }}
+                    data-aos="fade-up"
+                >
+                    Games to lose on:
+                    <BetCardGrid />
+                </Box>
+                <Box
+                    sx={{
+                        display: "flex",
+                        flexWrap: "wrap",
+                        justifyContent: "center",
+                        width: "90%",
+                    }}
+                    data-aos="fade-up"
+                >
+                    Games games games
+                    <BetCardGrid />
+                </Box>
+                <Box
+                    sx={{
+                        display: "flex",
+                        flexWrap: "wrap",
+                        justifyContent: "center",
+                        width: "90%",
+                    }}
+                    data-aos="fade-up"
+                >
+                    Some more games
+                    <BetCardGrid />
+                </Box>
+            </Box>
+        </div>
+    );
 };
 
 export default LandingPage;
