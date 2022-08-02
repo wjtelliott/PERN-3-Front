e1afe521e0c807258ca6735745d9991a30742732
prashantkumar1113 <prashantkumar1113@gmail.com>
2022-08-01 20:37:41 -0500

    added the mlb-team svgs to the dictionary

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { useState, useEffect } from "react";
+import {useState, useEffect} from "react";
 import Card from "@mui/material/Card";
 import Box from "@mui/material/Box";
 import CardActions from "@mui/material/CardActions";
@@ -7,36 +7,55 @@ import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import PlaceBetModal from "./PlaceBetModal";
 import "../App.css";
+import {MlbTeamPics} from "./Shared/MlbTeamPicDict";
 // import { testData } from "./Data-Handling/TestData";
 
 const GameCard = (props) => {
-  const [gameData, setGameData] = useState(props.gameData);
-  // console.log(gameData.bookmakers[0].markets[0].outcomes);
-  const [homeOdds, setHomeOdds] = useState(
-    gameData.bookmakers[0].markets[0].outcomes
-  );
-  console.log(homeOdds);
-  return (
-    <Card sx={{ maxWidth: 400, height: 300 }}>
-      <CardContent>
-        <Box display="grid" gridTemplateColumns="repeat(6, 1fr)" gap={2}>
-          <Box gridColumn="span 2">{gameData.home_team}</Box>
-          <Box gridColumn="span 2">VS</Box>
-          <Box gridColumn="span 2">{gameData.away_team}</Box>
-          {/* <Box gridColumn="span 2">{"home logo"}</Box>
+    const [gameData, setGameData] = useState(props.gameData);
+    // console.log(gameData.bookmakers[0].markets[0].outcomes);
+    const [homeOdds, setHomeOdds] = useState(
+        gameData.bookmakers[0].markets[0].outcomes
+    );
+    // console.log(MlbPics);
+    console.log(homeOdds);
+    return (
+        <Card
+            sx={{maxWidth: 400, height: 300, backgroundColor: "success.light"}}
+        >
+            <CardContent>
+                <Box
+                    display="grid"
+                    gridTemplateColumns="repeat(6, 1fr)"
+                    gap={2}
+                    // sx={{backgroundColor: "success.main"}}
+                >
+                    <Box gridColumn="span 2">{gameData.home_team}</Box>
+                    <img
+                        src={MlbTeamPics[gameData.home_team]}
+                        alt={gameData.home_team}
+                        height="50px"
+                    />
+                    <Box gridColumn="span 2">VS</Box>
+                    <Box gridColumn="span 2">{gameData.away_team}</Box>
+                    <img
+                        src={MlbTeamPics[gameData.away_team]}
+                        alt={gameData.away_team}
+                        height="50px"
+                    />
+                    {/* <Box gridColumn="span 2">{"home logo"}</Box>
           <Box gridColumn="span 2">{"away logo"}</Box> */}
-          {/* <Box gridColumn="span 2">team short Name team odds</Box> */}
-          <Box gridColumn="span 2"></Box>
+                    {/* <Box gridColumn="span 2">team short Name team odds</Box> */}
+                    <Box gridColumn="span 2"></Box>
 
-          <Box gridColumn="span 2">
-            <CardActions sx={{ display: "inline-block" }}>
-              <PlaceBetModal />
-            </CardActions>
-          </Box>
-        </Box>
-      </CardContent>
-    </Card>
-  );
+                    <Box gridColumn="span 2">
+                        <CardActions sx={{display: "inline-block"}}>
+                            <PlaceBetModal />
+                        </CardActions>
+                    </Box>
+                </Box>
+            </CardContent>
+        </Card>
+    );
 };
 
 export default GameCard;
