42a639eaa80859e5ce1b3c869635b8774888b089
wjtelliott <wjtelliott@gmail.com>
2022-08-01 23:07:49 -0500

    Trying to fix tabs

--- src/components/Profile/ProfileBets.jsx
+++ src/components/Profile/ProfileBets.jsx
@@ -8,141 +8,141 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { ApiUrls } from "../../context/APIContext";
 
 export default function ProfileBets() {
-	const [value, setValue] = useState(0);
+    const [value, setValue] = useState(0);
 
-	const [userBets, setUserBets] = useState([]);
-	const [isMobile, setIsMobile] = useState(false);
+    const [userBets, setUserBets] = useState([]);
+    const [isMobile, setIsMobile] = useState(false);
 
-	const apiUrls = useContext(ApiUrls);
-	const { user, isAuthenticated, isLoading } = useAuth0();
+    const apiUrls = useContext(ApiUrls);
+    const { user, isAuthenticated, isLoading } = useAuth0();
 
-	useEffect(() => {
-		setIsMobile(window.innerWidth < 800);
-	}, [window.innerWidth]);
+    useEffect(() => {
+        setIsMobile(window.innerWidth < 800);
+    }, [window.innerWidth]);
 
-	useEffect(() => {
-		(async () => {
-			if (isLoading || !isAuthenticated) return;
-			const response = await fetch(apiUrls.getUserBetsUrl(user.sub));
-			const resData = await response.json();
-			setUserBets(resData);
-		})();
-	}, [isLoading]);
+    useEffect(() => {
+        (async () => {
+            if (isLoading || !isAuthenticated) return;
+            const response = await fetch(apiUrls.getUserBetsUrl(user.sub));
+            const resData = await response.json();
+            setUserBets(resData);
+        })();
+    }, [isLoading]);
 
-	if (!isAuthenticated) return null;
+    if (!isAuthenticated) return null;
 
-	const handleChange = (_, newValue) => {
-		setValue(newValue);
-	};
+    const handleChange = (_, newValue) => {
+        setValue(newValue);
+    };
 
-	function a11yProps(index) {
-		return {
-			id: `bet-tab-${index}`,
-			"aria-controls": `bet-tabpanel-${index}`,
-		};
-	}
+    function a11yProps(index) {
+        return {
+            id: `bet-tab-${index}`,
+            "aria-controls": `bet-tabpanel-${index}`,
+        };
+    }
 
-	// This could be refactored to be more DRY
-	const currentUserGames = userBets
-		.filter((game) => !game.game_is_completed)
-		.map((game) => {
-			return (
-				<div>
-					<p>
-						Here is a game that is not completed:{" "}
-						{JSON.stringify(game)}
-					</p>
-				</div>
-			);
-		});
+    // This could be refactored to be more DRY
+    const currentUserGames = userBets
+        .filter((game) => !game.game_is_completed)
+        .map((game) => {
+            return (
+                <div>
+                    <p>
+                        Here is a game that is not completed:{" "}
+                        {JSON.stringify(game)}
+                    </p>
+                </div>
+            );
+        });
 
-	const previousUserGames = userBets
-		.filter((game) => game.game_is_completed)
-		.map((game) => {
-			return (
-				<div>
-					<p>
-						Here is a game that is completed: {JSON.stringify(game)}
-					</p>
-				</div>
-			);
-		});
+    const previousUserGames = userBets
+        .filter((game) => game.game_is_completed)
+        .map((game) => {
+            return (
+                <div>
+                    <p>
+                        Here is a game that is completed: {JSON.stringify(game)}
+                    </p>
+                </div>
+            );
+        });
 
-	const previousUserGamesLost = userBets
-		.filter((game) => game.game_is_completed && !game.bet_success)
-		.map((game) => {
-			return (
-				<div>
-					<p>
-						Here is a game that the bet was lost!:{" "}
-						{JSON.stringify(game)}
-					</p>
-				</div>
-			);
-		});
+    const previousUserGamesLost = userBets
+        .filter((game) => game.game_is_completed && !game.bet_success)
+        .map((game) => {
+            return (
+                <div>
+                    <p>
+                        Here is a game that the bet was lost!:{" "}
+                        {JSON.stringify(game)}
+                    </p>
+                </div>
+            );
+        });
 
-	const previousUserGamesWon = userBets
-		.filter((game) => game.game_is_completed && game.bet_success)
-		.map((game) => {
-			return (
-				<div>
-					<p>
-						Here is a game that the bet was won!:{" "}
-						{JSON.stringify(game)}
-					</p>
-				</div>
-			);
-		});
+    const previousUserGamesWon = userBets
+        .filter((game) => game.game_is_completed && game.bet_success)
+        .map((game) => {
+            return (
+                <div>
+                    <p>
+                        Here is a game that the bet was won!:{" "}
+                        {JSON.stringify(game)}
+                    </p>
+                </div>
+            );
+        });
 
-	return (
-		<Box sx={{ width: "100%" }}>
-			<Box
-				sx={{
-					borderBottom: 1,
-					borderColor: "divider",
-					justifyContent: "center",
-				}}
-			>
-				<Tabs
-					value={value}
-					onChange={handleChange}
-					aria-label="profile bet tabs"
-					sx={{ px: 2, d: "flex", justifyContent: "center" }}
-				>
-					<Tab label="Current Bets" {...a11yProps(0)} />
-					<Tab
-						sx={{
-							display: {
-								xs: "none",
-								sm: "none",
-								md: "inline-flex",
-							},
-						}}
-						label="Bet History"
-						{...a11yProps(1)}
-					/>
-					<Tab
-						label={(!isMobile ? "Previous " : "") + "Bets Won"}
-						{...a11yProps(2)}
-					/>
-					<Tab
-						label={(!isMobile ? "Previous " : "") + "Bets Lost"}
-						{...a11yProps(3)}
-					/>
-				</Tabs>
-			</Box>
-			<BetTab value={value} index={0}>
-				{currentUserGames}
-			</BetTab>
-			<BetTab value={value} index={1}>
-				{previousUserGames}
-			</BetTab>
-			<BetTab value={value} index={2}>
-				{previousUserGamesWon}
-			</BetTab>
-			<BetTab value={value} index={3}>
-				{previousUserGamesLost}
-			</BetTab>
-		</Box>
-	);
+    return (
+        <Box sx={{ width: "100%" }}>
+            <Box
+                sx={{
+                    borderBottom: 1,
+                    borderColor: "divider",
+                    justifyContent: "center",
+                }}
+            >
+                <Tabs
+                    value={value}
+                    onChange={handleChange}
+                    aria-label="profile bet tabs"
+                    sx={{ px: 2, d: "flex", justifyContent: "center" }}
+                >
+                    <Tab label="Current Bets" {...a11yProps(0)} />
+                    <Tab
+                        sx={{
+                            display: {
+                                xs: "none",
+                                sm: "none",
+                                md: "inline-flex",
+                            },
+                        }}
+                        label="Bet History"
+                        {...a11yProps(1)}
+                    />
+                    <Tab
+                        label={(!isMobile ? "Previous " : "") + "Bets Won"}
+                        {...a11yProps(2)}
+                    />
+                    <Tab
+                        label={(!isMobile ? "Previous " : "") + "Bets Lost"}
+                        {...a11yProps(3)}
+                    />
+                </Tabs>
+            </Box>
+            <BetTab value={value} index={0}>
+                {currentUserGames}
+            </BetTab>
+            <BetTab value={value} index={1}>
+                {previousUserGames}
+            </BetTab>
+            <BetTab value={value} index={2}>
+                {previousUserGamesWon}
+            </BetTab>
+            <BetTab value={value} index={3}>
+                {previousUserGamesLost}
+            </BetTab>
+        </Box>
+    );
 }
