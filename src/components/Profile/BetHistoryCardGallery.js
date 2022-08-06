98f8847829cf091ccf953c8b0a5e7be0da9fb22d
William Elliott <wjtelliott@gmail.com>
2022-08-05 23:34:10 -0500

    Implemented bet pagination. first pass

--- src/components/Profile/BetHistoryCardGallery.js
+++ src/components/Profile/BetHistoryCardGallery.js
@@ -1,8 +1,52 @@
-import {Box} from "@mui/material";
-import React from "react";
+import {Box, Button} from "@mui/material";
+import React, {useEffect, useState} from "react";
 import BetHistoryCard from "./BetHistoryCard";
+import {useAuth0} from "@auth0/auth0-react";
 
-const BetHistoryCardGallery = ({bets}) => {
+const BetHistoryCardGallery = ({betType}) => {
+    const [userBets, setUserBets] = useState([]);
+    const [amountLoaded, setAmountLoaded] = useState(0);
+    const [loadMoreVisible, setLoadMoreVisible] = useState(true);
+    const [loadMoreToggle, setLoadMoreToggle] = useState(false);
+    const {user, isLoading, isAuthenticated} = useAuth0();
+
+    useEffect(() => {
+        (async () => {
+            if (isLoading || !isAuthenticated || userBets.length > 0) return;
+            const amountToLoad = 10;
+            const url =
+                (process.env.REACT_APP_BACKEND_API_URL ??
+                    `http://localhost:3001`) +
+                `/bets/profile/${user.sub}/${amountLoaded}/${amountToLoad}/${betType}`;
+            const response = await fetch(url);
+            const resData = await response.json();
+            setAmountLoaded(amountLoaded + resData.length);
+            //if (resData.length < 10) setLoadMoreVisible(false);
+            console.log(resData);
+            setUserBets([...userBets, resData]);
+        })();
+    }, [isLoading]);
+
+    useEffect(() => {
+        (async () => {
+            if (isLoading || !isAuthenticated) return;
+            const amountToLoad = 10;
+            const url =
+                (process.env.REACT_APP_BACKEND_API_URL ??
+                    `http://localhost:3001`) +
+                `/bets/profile/${user.sub}/${amountLoaded}/${amountToLoad}/${betType}`;
+            const response = await fetch(url);
+            const resData = await response.json();
+            setAmountLoaded(amountLoaded + resData.length);
+            if (resData.length < 10) setLoadMoreVisible(false);
+            setUserBets([...userBets, ...resData]);
+        })();
+    }, [loadMoreToggle]);
+
+    const formattedUserBets = userBets.map((bet, index) => (
+        <BetHistoryCard key={`betHistoryCard${index}`} gameData={bet} />
+    ));
+
     return (
         <Box
             sx={{
@@ -11,9 +55,21 @@ const BetHistoryCardGallery = ({bets}) => {
                 justifyContent: "center",
             }}
         >
-            {bets.map((bet, index) => (
-                <BetHistoryCard key={`betHistoryCard${index}`} gameData={bet} />
-            ))}
+            <Box
+                sx={{
+                    display: "flex",
+                    flexWrap: "wrap",
+                    justifyContent: "center",
+                }}
+            >
+                {formattedUserBets}
+            </Box>
+
+            {loadMoreVisible && (
+                <Button onClick={() => setLoadMoreToggle(!loadMoreToggle)}>
+                    Load More
+                </Button>
+            )}
         </Box>
     );
 };
