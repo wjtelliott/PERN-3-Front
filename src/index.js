0df9e2ee36326a166d36113c7c3e1c93f4964947
prashantkumar1113 <prashantkumar1113@gmail.com>
2022-08-06 12:10:17 -0500

    changed the frontend url for auth0

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import {Auth0Provider} from "@auth0/auth0-react";
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-const redirectUri = process.env.REACT_APP_BACKEND_API_URL + "/profile/confirm";
+const redirectUri = process.env.REACT_APP_FRONTEND_URL + "/profile/confirm";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <Auth0Provider
